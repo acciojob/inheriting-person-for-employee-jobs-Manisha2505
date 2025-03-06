2ec4455abe9b9343b53a08dc435f7f8a31d24543
Manisha2505 <manishachandra.25@gmail.com>
2025-03-06 14:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,32 @@ Person.prototype.greet = function () {
 
 function Employee(name, age, jobTitle) {
     Person.call(this, name, age); // Inherit properties from Person
-    this.jobTitle = jobTitle;
+    this.jobTitle = jobTitle;class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
+
 }
 
 // Inherit methods from Person
